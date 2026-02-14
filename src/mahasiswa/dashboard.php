27a8ae8ddcb4bdce8c67540f762d365c16f6074a
arifdwifebrianto <arifdwfe@gmail.com>
2026-02-15 06:34:16 +0700

    update mahasiswa dashboard

---
+++ src/mahasiswa/dashboard.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = "Dashboard";
+$activeNav = "home";
+require_once __DIR__ . "/../templates/header.php";
+?>
+
+<!-- KOSONG dulu, nanti isi dashboard di sini -->
+
+<?php require_once __DIR__ . "/../templates/footer.php"; ?>
